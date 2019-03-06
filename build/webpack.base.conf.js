9b1d8a20344fb76009d4803fc6c524f4f03c567c
terry <472283504@qq.com>
2019-03-06 19:07:37 +0800

    update

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,13 +3,15 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
+  mode: 'development',
   context: path.resolve(__dirname, '../'),
   entry: {
     app: './src/main.js'
@@ -17,9 +19,8 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    publicPath: process.env.NODE_ENV === 'production'
-      ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+    publicPath: process.env.NODE_ENV === 'production' ?
+      config.build.assetsPublicPath : config.dev.assetsPublicPath
   },
   plugins: [
     // make sure to include the plugin for the magic
@@ -33,9 +34,20 @@ module.exports = {
       '#': resolve('src/components')
     }
   },
+  optimization: {
+    splitChunks: {
+      name: 'commons',
+      chunks: 'all',
+      minChunks: 2
+    },
+    minimizer: [new TerserPlugin({
+      test: /\.js(\?.*)?$/i,
+      exclude: /node_modules/,
+      parallel: true
+    })]
+  },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
@@ -44,7 +56,7 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader?cacheDirectory=true',
         exclude: /node_modules/,
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve('src'), resolve('test')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
