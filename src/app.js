483e508272d6bbd0721695f0e9d961305c4f4329
terrylin <472283504@qq.com>
2020-01-10 15:13:10 +0800

    update

--- src/app.js
+++ src/app.js
@@ -5,9 +5,13 @@ var server = ws.createServer(function (conn) {
     conn.on("text", function (str) {
         console.log("Received "+str)
         conn.sendText(str)
+        boardcast(str)
     })
     conn.on("close", function (code, reason) {
         console.log("Connection closed")
+    })
+    conn.on('onMessage', (data) => {
+        conn.sendText(data)
     })
 }).listen(8001)
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,54 @@
 <template>
-    <div class="hello">
-        <input type="text" name="" v-model="sendMsg">
-        <button type="button" name="button" @click="handleSendMsg">发送</button>
-        <div class="" v-for="(item, index) in ws" :key="index">
-            {{item.msg}}
-        </div>
-    </div>
+<div class="hello">
+	<input type="text" name="" v-model="sendMsg">
+	<button type="button" name="button" @click="handleSendMsg">发送</button>
+	<div class="" v-for="(item, index) in ws" :key="index">
+		{{item.msg}}
+	</div>
+</div>
 </template>
 
 <script>
 /* eslint-disable */
 export default {
 	name: 'HelloWorld',
-    data() {
-        return {
-            count: 0,
-            sendMsg: '',
-            ws: []
-        }
+	data() {
+		return {
+			count: 0,
+			sendMsg: '',
+			ws: [],
+            websock: ''
+		}
+	},
+	sockets: {
+		connect: function() {},
+		message: function(data) {
+			this.ws.push(data)
+		}
+	},
+    mounted () {
+            this.initWebsocket()
     },
-    sockets: {
-       connect: function () {
-       },
-       message: function (data) {
-          this.ws.push(data)
-       }
-   },
-    methods: {
-        handleSendMsg () {
-            this.$socket.emit('message', {
-                msg: this.sendMsg
-            })
-        }
-    }
+	methods: {
+		handleSendMsg() {
+			this.$socket.emit('message', {
+				msg: this.sendMsg
+			})
+
+            this.websock.send(this.sendMsg)
+		},
+		initWebsocket() {
+			const wsuri = `ws://localhost:8001` //这个地址由后端童鞋提供
+			this.websock = new WebSocket(wsuri);
+			this.websock.onmessage = this.webSocketOnMessage;
+			this.websock.onopen = this.websocketonopen;
+			this.websock.onerror = this.websocketonerror;
+			this.websock.onclose = this.websocketclose;
+		},
+		webSocketOnMessage(e) {
+            console.log('wori', e.data);
+		},
+	}
 }
 </script>
 
